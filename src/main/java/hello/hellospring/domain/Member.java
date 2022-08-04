fdfdfde805e8c8b088cca68dbc9c95446286ea21
twoonezero <dnjsdudtm98@naver.com>
2022-08-04 12:26:20 +0900

    통합테스트, JdbcTemplate, JPA, Spring_Data_Jpa

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,9 +1,17 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+//jpa 가 관리하는 엔티티
+@Entity
 public class Member {
 
+    //@GeneratedValue ID 를 자동으로 생성하는 것읠 IDENTITY 전략이라고 함
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
 
     public Long getId() {
         return id;
